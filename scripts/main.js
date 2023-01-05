265302fda41f314e0eabacec4375f885f1e620ed
JakeTurnick <jaketurnick@gmail.com>
2023-01-05 15:23:04 -0500

    refactored for use strict - started work on calculation

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,44 @@
-const numbers = document.querySelectorAll(".number");
-const operators = document.querySelectorAll(".operator, .clear");
-const equals = document.querySelector(".equal-sign");
-console.log(equals)
+(function() {
+    "use strict'"
+
+    const numbers = document.querySelectorAll(".number");
+    const operators = document.querySelectorAll(".operator, .clear");
+    const equals = document.querySelector(".equal-sign");
+
+    let calculation = [];
 
 
-//Alerts the number pressed for each number button
-numbers.forEach(number => {
-    number.addEventListener('click', (e) => {
-        pushNumber(e);
-        console.log(e.target.value);
-    })
-});
-function pushNumber(e) {
-    alert(e.target.value);
-}
 
-//Alerts for each operators
-operators.forEach(op => {
-    op.addEventListener('click', (e) => {
-        pushOperator(e);
+    //Alerts the number pressed for each number button
+    numbers.forEach(number => {
+        number.addEventListener('click', (e) => {
+            pushNumber(e);
+            calculation.push(e.target.value);
+            console.log(calculation);
+        })
     });
-})
-function pushOperator(e) {
-    alert(e.target.value);
-}
+    function pushNumber(e) {
+        alert(e.target.value);
+        // calculation.push(e.target.value);
+        // console.log(calculation);
+    }
 
-equals.addEventListener('click', (e) => {
-    calculate(e);
-})
-function calculate(e) {
-    alert(e.target.value);
-}
+    //Alerts for each operators
+    operators.forEach(op => {
+        op.addEventListener('click', (e) => {
+            pushOperator(e);
+        });
+    })
+    function pushOperator(e) {
+        alert(e.target.value);
+        calculation.push(e.target.value);
+    }
+
+    equals.addEventListener('click', (e) => {
+        calculate(e);
+    })
+    function calculate(e) {
+        alert(e.target.value);
+        alert(calculation);
+    }
+})();
