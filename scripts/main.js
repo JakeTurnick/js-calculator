a512a23490421a17754691d311dd3737176bed00
JakeTurnick <jaketurnick@gmail.com>
2023-01-05 15:13:01 -0500

    calculate alerts when = button is pressed

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator, .clear");
-console.log(operators)
+const equals = document.querySelector(".equal-sign");
+console.log(equals)
+
 
 //Alerts the number pressed for each number button
 numbers.forEach(number => {
@@ -20,5 +22,12 @@ operators.forEach(op => {
     });
 })
 function pushOperator(e) {
+    alert(e.target.value);
+}
+
+equals.addEventListener('click', (e) => {
+    calculate(e);
+})
+function calculate(e) {
     alert(e.target.value);
 }
